5c80ae7628a8592e42ecdd193e3e53c5bc4a0ed5
AlexanderTobert <tobert.sasha@gmail.com>
2023-12-18 21:00:44 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,67 +8,35 @@ import css from './maim.module.css'
 class App extends Component {
   state = {
     contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
-    name: '',
-    number: ''
+  };
+
+handleAddProfile = formData => {
+  const hasDuplicates = this.state.contacts.some(
+    profile => profile.name === formData.name
+  );
+
+  if (hasDuplicates) {
+    Notify.alert(`Profile with name ${formData.name} already exists!`);
+    return;
   }
 
-  handleFormSubmit = event => {
-    event.preventDefault();
-
-    const name = event.currentTarget.elements.name.value;
-    const number = event.currentTarget.elements.number.value;
-
-    if (this.state.contacts.find(e => e.name === name)) {
-      Notify.warning(`${name} is already in contacts`);
-      return;
-    }
-
-    if (isNaN(number)) {
-      Notify.failure('Please enter a valid number.');
-      return;
-    }
-
-    if (/\d/.test(name)) {
-      Notify.failure('Name should not contain numbers.');
-      return;
-    }
-
-    const formData = {
-      name,
-      number: Number(number),
-      id: nanoid()
-    };
-
-    this.handleAddProfile(formData);
-    event.currentTarget.reset();
+  const finalProfile = {
+    ...formData,
+    id: nanoid(),
   };
 
-  handleAddProfile = formData => {
-    const hasDuplicates = this.state.contacts.some(
-      profile => profile.name === formData.name
-    );
-    if (hasDuplicates) {
-      alert(`Profile with name ${formData.name} already exists!`);
-      return;
-    }
-
-
-    const finalProfile = {
-      ...formData,
+  this.setState(prevState => {
+    return {
+      contacts: [...prevState.contacts, finalProfile],
     };
-
-    this.setState(prevState => {
-      return {
-        contacts: [...prevState.contacts, finalProfile],
-      };
-    });
-  };
+  });
+};
 
   handleChangeFilter = event => {
     const value = event.target.value;
@@ -81,13 +49,7 @@ class App extends Component {
     }));
   };
 
-  handleInputChange = (event) => {
-  const { name, value } = event.target;
-
-  this.setState({
-      [name]: value,
-  });
-};
+  
 
   render() {
     return (
@@ -95,10 +57,9 @@ class App extends Component {
         <h2 style={{fontSize: '2em', fontWeight: 700,
     marginBottom: '10px',}}>Phone number</h2>
         <ContactForm
-          handleSubmit={this.handleFormSubmit}
-          formData={{ name: this.state.name, number: this.state.number }}
-          handleInputChange={this.handleInputChange}
-        />
+  handleAddProfile={this.handleAddProfile}
+  contacts={this.state.contacts}
+/>
 
         <h2 style={{fontSize: '2em', fontWeight: 700,
     marginBottom: '10px',}}>Contacts</h2>
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,37 +1,82 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Notify } from 'notiflix';
 import css from './ContactForm.module.css'
 
-const ContactForm = ({ handleSubmit, formData, handleInputChange }) => {
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    const { name, number } = this.state;
+    const { handleAddProfile } = this.props;
+
+    if (this.props.contacts.find(e => e.name === name)) {
+      Notify.warning(`${name} уже есть в контактах`);
+      return;
+    }
+
+    if (isNaN(number)) {
+      Notify.failure('Пожалуйста, введите корректный номер.');
+      return;
+    }
+
+    if (/\d/.test(name)) {
+      Notify.failure('Имя не должно содержать цифры.');
+      return;
+    }
+
+    const formData = {
+      name,
+      number: Number(number),
+    };
+
+    handleAddProfile(formData);
+    this.setState({ name: '', number: '' });
+  };
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  render() {
     return (
-        <form className={css.form} onSubmit={handleSubmit}>
-            <label className={css.formLabel} htmlFor="name">
-                <span>Name:</span>
-                <input
-                    type="text"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleInputChange}
-                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                    required
-                />
-            </label>
-            <label className={css.formLabel} htmlFor="">
-            <span>Number:</span>
-                <input
-                    type="tel"
-                    name="number"
-                    value={formData.number}
-                    onChange={handleInputChange}
-                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                    required
-                />
-            </label>
-            <button type="submit">Add Profile</button>
-        </form>
+      <form className={css.form} onSubmit={this.handleFormSubmit}>
+        <label className={css.formLabel} htmlFor="name">
+          <span>Name:</span>
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleInputChange}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <label className={css.formLabel} htmlFor="number">
+          <span>Number:</span>
+          <input
+            type="tel"
+            name="number"
+            value={this.state.number}
+            onChange={this.handleInputChange}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <button type="submit">Add Profile</button>
+      </form>
     );
-};
+  }
+}
 
-
-export { ContactForm }
+export { ContactForm };
